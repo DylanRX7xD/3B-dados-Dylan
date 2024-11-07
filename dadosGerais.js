d634c5663fba107b545ab833cf1a510ec2b34999
Dylan Rojas <105813600+DylanRX7xD@users.noreply.github.com>
2024-11-07 10:15:14 -0300

    Add files via upload

--- dadosGerais.js
+++ dadosGerais.js
@@ -1,3 +1,3 @@
-const url = 'https://github.com/DylanRX7xD/Projeto-Pesquisa-3B.git'
+const url = "https://raw.githubusercontent.com/DylanRX7xD/Projeto-Pesquisa-3B/refs/heads/main/Projeto-Futebol-3B-"  
 
 console.log(url)
