69c49189cee84d6016e555273dc4a5d506061b05
Dylan Rojas <105813600+DylanRX7xD@users.noreply.github.com>
2024-11-14 10:46:17 -0300

    Add files via upload

--- dadosGerais.js
+++ dadosGerais.js
@@ -1,3 +1,11 @@
 const url = "https://raw.githubusercontent.com/DylanRX7xD/Projeto-Pesquisa-3B/refs/heads/main/Projeto-Futebol-3B-"  
 
-console.log(url)
+async function visualizarInformacoesGerais() {
+    const res = await fetch(url)
+    const dados = await res.json
+    const totalEntrevistados = (dados.esporte_norte)
+
+
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('caixa-grafico_texto')
+    paragrafo.innerHTML = `Entrevistamos <span>${totalEntrevistados} milhão</span> de brasileiros para descobrir quais são os esportes mais praticados no país`
