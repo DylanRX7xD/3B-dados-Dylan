f6a3cc678d0035fa403cc820ed92ba4715d81500
Dylan Rojas <105813600+DylanRX7xD@users.noreply.github.com>
2024-11-22 08:39:21 -0300

    Add files via upload

--- dadosGerais.js
+++ dadosGerais.js
@@ -1,11 +1,40 @@
-const url = "https://raw.githubusercontent.com/DylanRX7xD/Projeto-Pesquisa-3B/refs/heads/main/Projeto-Futebol-3B-"  
+const url= 'https://raw.githubusercontent.com/DylanRX7xD/Projeto-Pesquisa-3B/refs/heads/main/esportes_mais_praticados.json'
 
-async function visualizarInformacoesGerais() {
+
+
+async function vizualizando(){
     const res = await fetch(url)
-    const dados = await res.json
-    const totalEntrevistados = (dados.esporte_norte)
+    const dados = await res.json()
+
+     
+    const esportes = Object.keys(dados)
+    const votos = Object.values(dados)
+    const esporteMaisVotado = esportes[0]
+    const quantidadeDeVoto = Object.values(dados) [0]
 
 
-    const paragrafo = document.createElement('p')
+    const data = [
+        {
+            x:esportes,
+            y:votos,
+            type: 'bar'
+        }
+    ]
+
+
+    let paragrafo = document.createElement('p')
+
     paragrafo.classList.add('caixa-grafico_texto')
-    paragrafo.innerHTML = `Entrevistamos <span>${totalEntrevistados} milhão</span> de brasileiros para descobrir quais são os esportes mais praticados no país`
+
+    paragrafo.innerHTML = `Nesta pesquisa, buscou-se compreender qual o esporte mais praticado pela população brasileira. 0 ${esporteMaisVotado} foi o esporte mais votado entre todos, com um total de ${quantidadeDeVoto} votos, em uma pesquisa que teve um total de 4350 participantes.`
+
+    let caixa = document.getElementById('caixa-grafico')
+    caixa.appendChild(paragrafo)
+
+    const grafico = document.createElement('div')
+    grafico.className = 'grafico'
+    document.getElementById('caixa-grafico').appendChild(grafico)
+    Plotly.newPlot(grafico, data)
+}
+
+vizualizando()
